a5436236f7f46bbaaa78c3e152a458498535b9c2
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-12-11 21:15:45 +0900

    Fix user search condition when condition is null

--- src/main/kotlin/trying/cosmos/domain/user/repository/UserRepositoryCustom.kt
+++ src/main/kotlin/trying/cosmos/domain/user/repository/UserRepositoryCustom.kt
@@ -1,8 +1,15 @@
 package trying.cosmos.domain.user.repository
 
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import trying.cosmos.domain.user.dto.request.UserSearchCondition
+import trying.cosmos.domain.user.entity.User
+
 interface UserRepositoryCustom {
 
     fun existsByEmail(email: String): Boolean
 
     fun existsByIdentifier(identifier: String): Boolean
+
+    fun findAllBy(condition: UserSearchCondition, pageable: Pageable): Slice<User>
 }
--- src/main/kotlin/trying/cosmos/domain/user/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/trying/cosmos/domain/user/repository/UserRepositoryImpl.kt
@@ -1,8 +1,15 @@
 package trying.cosmos.domain.user.repository
 
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import trying.cosmos.domain.user.dto.request.UserSearchCondition
 import trying.cosmos.domain.user.entity.QEmailUser.emailUser
 import trying.cosmos.domain.user.entity.QSocialUser.socialUser
+import trying.cosmos.domain.user.entity.QUser.user
+import trying.cosmos.domain.user.entity.User
+import trying.cosmos.global.extension.toSlice
 
 class UserRepositoryImpl(
 
@@ -28,5 +35,21 @@ class UserRepositoryImpl(
             .fetchOne()
 
         return user != null
+    }
+
+    override fun findAllBy(condition: UserSearchCondition, pageable: Pageable): Slice<User> {
+        val contents = queryFactory.selectFrom(user)
+            .where(
+                nameCondition(condition.name)
+            )
+            .offset(pageable.offset)
+            .limit(pageable.pageSize + 1L)
+            .fetch()
+
+        return contents.toSlice(pageable)
+    }
+
+    private fun nameCondition(name: String?): BooleanExpression? {
+        return user.name.eq(name ?: return null)
     }
 }
